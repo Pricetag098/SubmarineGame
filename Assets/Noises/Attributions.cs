b2d061c341a8d0b78dfde802643b4f3f0a95f377
Muglamp <99298207+Muglamp@users.noreply.github.com>
2026-02-17 20:49:16 +1030

    noises

---
+++ Assets/Noises/Attributions.cs
@@ -0,0 +1,7 @@
+
+//everything what isn't footsteps from GDC audiobundle
+//licence
+//https://sonniss.com/gdc-bundle-license/
+
+//footsteps by NOX sound
+//licence CC0
---
+++ Assets/Scirpts/SoundPlayer.cs
@@ -0,0 +1,26 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> clips;
+    [SerializeField] float pitchVariance;
+
+    AudioSource source;
+    float basePitch;
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        basePitch = source.pitch;
+    }
+
+    public void Play()
+    {
+        AudioClip clip = clips[Random.Range(0,clips.Count)];
+        source.pitch = basePitch + Random.Range(-pitchVariance, pitchVariance);
+        source.PlayOneShot(clip);
+
+    }
+}
